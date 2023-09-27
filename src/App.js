b0a35d029a4b5ed1d6bd1d55625a08b7d96fdd85
PartridgeAndOrange <m.partridgeandorange@gmail.com>
2023-09-27 23:27:22 +0200

    routing funkar nu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Button from "../src/components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+			<Routes>
+				<Route path="/" element={< Home />} />
+				<Route path="/about" element={< About />} />
+				<Route path="/contact" element={< Contact />} />
+			</Routes>
+    );
 }
 
 export default App;
