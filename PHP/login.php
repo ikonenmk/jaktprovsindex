579a00bb9a79d626b053997457f05783adbbf935
ikonenmk <markus.ikonen.karlstrom@gmail.com>
2023-12-19 06:35:11 +0100

    Lagt till hashade lösenord

--- PHP/login.php
+++ PHP/login.php
@@ -8,19 +8,23 @@
 	$_POST = json_decode(file_get_contents("php://input"),true);
 	$posted_username = $_POST['username'];
 	$posted_password = $_POST['password'];
+	$hashed_password = password_hash($posted_password, PASSWORD_DEFAULT);
+
 	
-	//Hämta kolumner username och role från tabellen users som uppfyller villkor för username och password
-	if($stmt = $conn->prepare("SELECT username, role FROM users WHERE username = ? AND password = ?")) {
+	//Hämta kolumner username och role från tabellen users som uppfyller villkor för username
+	if ($stmt = $conn->prepare("SELECT username, password, role FROM users WHERE username = ?")) {
+
 
 		//Bind parametrar
-		$stmt->bind_param("ss", $posted_username, $posted_password);
+		$stmt->bind_param("s", $posted_username);
 		//Genomför statement
 		$stmt->execute();
 		$stmt->store_result(); // Store the result to check the number of rows.
 		if ($stmt->num_rows > 0) {
-			$stmt->bind_result($username, $role);
+			$stmt->bind_result($username,$hashed_password, $role);
 			 $stmt->fetch(); // Fetch the data from the result.
-
+			
+			if (password_verify($posted_password, $hashed_password)) { //Check if password is correct
 			$userArray = array('username' => $username, 'role' => $role);
 
 		
@@ -34,9 +38,15 @@
 			header('Content-type: application/json');
 			echo json_encode($userArray);
 
-		} else { //Om hund inte finns meddela frontend
+		} else {
+			// Password is incorrect
 			echo "Username or password is wrong";
 		}
-		$stmt->close();
-		}
+	} else {
+		// User does not exist
+		echo "Username or password is wrong";
+	}
+	$stmt->close();
+	}
+		
 ?>
